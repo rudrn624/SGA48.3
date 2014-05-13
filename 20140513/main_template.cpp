dceb40bb2044d7af2a8b949974c02309a8706245
jklim <gocobe1228@naver.com>
2014-05-13 17:51:42 +0900

    템플릿
    .

---
+++ 20140513/main_template.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+template<typename T1, 
+		 typename T2> 
+void MySwap(T1& a, T2& b)
+{
+	T2 c = a;
+	a = b;
+	b = c;
+}
+
+
+int main(void)
+{
+	int* pA = NULL;
+
+	if (pA == NULL)
+	{
+		pA = new int;
+	}
+
+	// todo...
+
+
+	SafeDelete(pA);
+	//if (pA)
+	//{
+	//	delete pA;
+	//	pA = NULL;
+	//}
+
+	cout 
+
+
+	_getch();
+	return 0;
+}
